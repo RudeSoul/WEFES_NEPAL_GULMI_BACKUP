cf0924d3e7a346ea83a372b428cb2610115026a6
rudesoul <prabesh7@gmail.com>
2026-09-23 15:35:08 +0545

    refactor(web): remove palika dossier export modal barrel export

--- apps/web/src/features/palika/index.ts
+++ apps/web/src/features/palika/index.ts
@@ -1,7 +1,6 @@
 export * from './DistrictDetail';
 export * from './PalikaQuickMatrix';
 export * from './PalikaBenchmarkingWidget';
-export * from './PalikaDossierExportModal';
 export * from './PalikaHoverCard';
 export * from './CropComparativeAnalysis';
 export * from './FeasibilityMatrix';
